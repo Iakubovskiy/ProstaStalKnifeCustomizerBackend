a69a84e2d37ce8ef08249280fe100599b80666ae
Iakuba <diman.150106@gmail.com>
2025-02-13 18:00:03 +0200

    notification service corrected

--- WorkshopBackend/Services/EmailSenderService.cs
+++ WorkshopBackend/Services/EmailSenderService.cs
@@ -17,14 +17,14 @@ namespace WorkshopBackend.Services
         public async Task SendEmailAsync(EmailDTO emailDto)
         {
             var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse(_configuration.GetSection("SenderEmail").Value));
+            email.From.Add(MailboxAddress.Parse(_configuration.GetValue<string>("SenderEmail")));
             email.To.Add(MailboxAddress.Parse(emailDto.EmailTo));
             email.Subject = emailDto.EmailSubject;
             email.Body = new TextPart(TextFormat.Plain){Text = emailDto.EmailBody };
 
             using var smtp = new SmtpClient();
-            await smtp.ConnectAsync(_configuration.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
-            await smtp.AuthenticateAsync(_configuration.GetSection("SenderEmail").Value, _configuration.GetSection("SenderEmailPassword").Value);
+            await smtp.ConnectAsync(_configuration.GetValue<string>("EmailHost"), 587, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_configuration.GetValue<string>("SenderEmail"), _configuration.GetValue<string>("SenderEmailPassword"));
             await smtp.SendAsync(email);
             await smtp.DisconnectAsync(true);
         }
