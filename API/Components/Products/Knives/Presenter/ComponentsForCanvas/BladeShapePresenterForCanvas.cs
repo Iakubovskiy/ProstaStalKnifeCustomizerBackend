1d25c44c38e08e8335d7f4b591d2b956c584a9e8
Iakuba <diman.150106@gmail.com>
2025-06-23 05:13:08 +0300

    shape presenter fixed

--- API/Components/Products/Knives/Presenter/ComponentsForCanvas/BladeShapePresenterForCanvas.cs
+++ API/Components/Products/Knives/Presenter/ComponentsForCanvas/BladeShapePresenterForCanvas.cs
@@ -11,11 +11,12 @@ public class BladeShapePresenterForCanvas
     public Guid Id { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
+    public FileEntity? BladeShapeImage { get; set; }
     public FileEntity BladeShapeModel { get; set; }
     public FileEntity? SheathModel { get; set; }
     public BladeShapeType ShapeType { get; set; }
     public Guid? SheathId {get; set;}
-    public BladeCharacteristics bladeCharacteristicsModel { get; set; }
+    public BladeCharacteristics BladeCharacteristicsModel { get; set; }
 
     public static async Task<BladeShapePresenterForCanvas> Present(
         BladeShape bladeShape, 
@@ -28,7 +29,7 @@ public class BladeShapePresenterForCanvas
         bladeShapePresenterForCanvas.Id = bladeShape.Id;
         bladeShapePresenterForCanvas.Name = bladeShape.Name.GetTranslation(locale);
         bladeShapePresenterForCanvas.BladeShapeModel = bladeShape.BladeShapeModel;
-        bladeShapePresenterForCanvas.bladeCharacteristicsModel = bladeShape.BladeCharacteristics;
+        bladeShapePresenterForCanvas.BladeCharacteristicsModel = bladeShape.BladeCharacteristics;
         if(bladeShape.Sheath != null)
         {
             bladeShapePresenterForCanvas.SheathModel = bladeShape.Sheath.Model;
@@ -36,6 +37,7 @@ public class BladeShapePresenterForCanvas
         bladeShapePresenterForCanvas.Price = await priceService.GetPrice(bladeShape.Price, currency);
         bladeShapePresenterForCanvas.ShapeType = bladeShape.Type;
         bladeShapePresenterForCanvas.SheathId = bladeShape.Sheath?.Id;
+        bladeShapePresenterForCanvas.BladeShapeImage = bladeShape.BladeShapePhoto;
         
         return bladeShapePresenterForCanvas;
     }
